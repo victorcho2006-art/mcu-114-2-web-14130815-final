418760c5aa08379d140ea01d4adcc2419dc1c25b
卓定騰 <victorcho2006@gmail.com>
2026-06-10 17:20:02 +0800

    feat(product): 利用事件繫結設定產品顯示狀態

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,4 +16,8 @@ export class App {
   isShow = true;
 
   photoUrl = 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img';
+  
+  onSetDisplay(isShow: boolean): void {
+    this.isShow = isShow;
+  }
 }
